89e2332ffd38bdeb55118e74794f1ade481cf92b
Justice Baum <baumj2@wwu.edu>
2023-04-29 13:46:57 -0700

    Bank skeleton written

---
+++ Bank.java
@@ -0,0 +1,40 @@
+public class Bank {
+    // Fields
+    int[] dollars;
+    int[] credits;
+
+    // Methods
+
+    // Constructor
+    // Take an array of players and the number thereof
+    // to determine starting currency/currencies
+    public Bank Bank(int[] players, int numPlayers) {
+        return null;
+    }
+
+    public void setDollars(int player, int amount) {
+        return;
+    }
+
+    public void setCredits(int player, int amount) {
+        return;
+    }
+
+    public int getDollars(int player) {
+        return 0;
+    }
+
+    public int getCredits(int player) {
+        return 0;
+    }
+
+    // add given amount(s) to specified player
+    public void add(int player, int dollars, int credits) {
+        return;
+    }
+
+    // subtract given amount(s) to specified player
+    public void pay(int player, int dollars, int credits) {
+        return;
+    }
+}
