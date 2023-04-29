d8537288fc1727e38bac7a209b60dd66d05f215e
Ellie Skiffington <skiffie@wwu.edu>
2023-04-29 14:58:24 -0700

    location skeleton

---
+++ Location.java
@@ -0,0 +1,42 @@
+public class Location{
+    private String name;
+    private Location[] neighbors;
+    private int[] players;
+
+    //constructor
+    public Location(){
+
+    }
+    public Location(String name){
+
+    }
+
+    //getters and Setters 
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setNeighbors(Location[] newNeighbors){
+        neighbors = newNeighbors;
+    }
+
+    public Location[] getNeighbors(){
+        return neighbors;
+    }
+    public void setPlayer(int player, int value){
+        players[player] = value;
+    }
+    public int[] getPlayers(){
+        return players;
+    }
+
+    //class methods
+    public boolean checkIfNeighbor(Location l){
+        return false;
+    }
+
+}
