cc018abf44b63ae02a03b3b9384c37e0e24ba913
Ellie Skiffington <skiffie@wwu.edu>
2023-04-29 15:19:58 -0700

    Location Manager Skeleton Code

---
+++ LocationManager.java
@@ -0,0 +1,27 @@
+public class LocationManager{
+    private Board board;
+
+    public LocationManager(){
+
+    }
+    
+    public void setBoard(Board newBoard){
+        board=newBoard;
+    }
+    public Board getBoard(){
+        return Board;
+    }
+
+    public boolean checkCardStatus(Scene s){
+        return false;
+    }
+    public boolean checkRoleStatus(Scene s, Role r){
+        return false;
+    }
+    public boolean checkMove(Location start, Location end, Player p){
+        return false;
+    }
+    public boolean checkLocation(Location l, Player p){
+        return false;
+    }
+}
