980fcbe3a78f26855710ca4920c06efecbe8e75b
Justice Baum <baumj2@wwu.edu>
2023-04-29 13:55:14 -0700

    Board skeleton

---
+++ Board.java
@@ -0,0 +1,64 @@
+public class Board {
+    // Fields
+    Scene[] scene;
+    Location trailer;
+    Location castingOffice;
+    int cardsLeft;
+    Card[] deck;
+
+    // Methods
+    public Board Board() {
+        return null;
+    }
+
+    public void setScene(int index, Scene scene) {
+        return;
+    }
+
+    // Returns scene at specified index
+    public Scene getScene(int index) {
+        return;
+    }
+
+    public void setTrailer(Location loc) {
+        return;
+    }
+
+    public Location getTrailer() {
+        return;
+    }
+
+    public void setCastingOffice(Location loc) {
+        return;
+    }
+
+    public Location getCastingOffice() {
+
+    }
+
+    public void setCardsLeft(int amount) {
+        return;
+    }
+
+    public int getCardsLeft() {
+        return;
+    }
+    
+    public void setCards(Card[] cards) {
+        return;
+    }
+
+    public Card[] getCards() {
+        return null;
+    }
+
+    // Get first card out of cards array
+    public Card pop() {
+        return null;
+    }
+
+    // Reset board for new game
+    public void reset() {
+        return;
+    }
+}
