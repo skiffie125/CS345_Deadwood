e78e637b8cdc11075b1809d048d7d690abec6774
Ellie Skiffington <skiffie@wwu.edu>
2023-05-11 19:11:07 -0700

    LocationManager methods implemented

--- src/LocationManager.java
+++ src/LocationManager.java
@@ -22,12 +22,45 @@ public class LocationManager{
         return s.cardFaceUp();
     }
     public boolean checkRoleStatus(Scene s, Role r){
-        return false;
+        boolean onScene =false;
+        boolean roleAvalible = false;
+        if (r.getPlayer() == null){
+            roleAvalible = true;
+        }
+        Role[] offCardRoles = s.getOffCardRoles();
+        Role[] onCardRoles = s.getCard().getOnCardRoles();
+        for(int i = 0; i < offCardRoles.length; i++){
+            if (r == offCardRoles[i]){
+                onScene = true;
+            }
+        }
+        for(int i = 0; i < onCardRoles.length; i++){
+            if (r == onCardRoles[i]){
+                onScene = true;
+            }
+        }
+        return onScene && roleAvalible;
     }
-    public boolean checkMove(Location start, Location end, Player p){
-        return false;
+    public boolean checkMove(Location start, Location end, int playerID){
+        boolean result = false;
+        if (start.checkIfNeighbor(end) && end.checkIfNeighbor(start)){
+            int[] players = start.getPlayers();
+            if (players[playerID] == 1){
+                result = true;
+            }
+        }
+        return result;
     }
+
+    //does this location have the player 
     public boolean checkLocation(Location l, int playerID){
-        return false;
+        int[] players = l.getPlayers();
+        if (players[playerID] == 1){
+            return true;
+        } else{
+            return false;
+        }
+
+        
     }
 }
