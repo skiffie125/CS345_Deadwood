3c119e22c68e5891c40a327f8b4813ab2997ba45
Justicebaum12 <baumj2@wwu.edu>
2023-05-14 00:46:45 -0700

    remove comments from viewer

--- src/Viewer.java
+++ src/Viewer.java
@@ -7,7 +7,6 @@ public class Viewer {
     static Scanner s = new Scanner(System.in);
     
     // Variable Fields
-    //private String buffer; <- with the current implementation of playText this can be eliminated
     private ProgressManager game;
 
     // Methods
@@ -17,62 +16,6 @@ public class Viewer {
     }
     public Viewer() {
     }
-
-    // Do we want this just to get the command or should we have it act on said commands too?
-    // Evidently i vote act on them
-    // But its an easy enough refactor
-    // -JB
-    public void playText() {
-        // If we do go the "this acts on the commands too" route
-        // Buffer can be eliminated as a field
-        String result = null;
-        Scanner s = new Scanner(System.in);
-        Player[] players = game.getPlayers();
-        while (true) {
-            result = s.nextLine();
-            //buffer += result;
-            if (result.equals("End game")) {
-                System.out.println("Game ended!");
-                break;
-            }
-            switch (result) {
-                case "Current":
-                    System.out.println("Current!");
-                    break;
-                case "Locate all":
-                    int numPlayers = players.length;
-                    for (int i = 0; i < numPlayers; i++) {
-                        System.out.println("Current player: Player " + players[i].getId());
-                        System.out.println("Player location: " + players[i].getLocation());
-                    }
-                    break;
-                case "Move":
-                    System.out.println("Move");
-                    break;
-                case "Work":
-                    System.out.println("Work");
-                    break;
-                case "Upgrade":
-                    System.out.println("Upgrade");
-                    break;
-                case "Rehearse":
-                    System.out.println("Rehearse");
-                    break;
-                case "Act":
-                    System.out.println("Act");
-                    break;
-                case "End turn":
-                    System.out.println("End turn");
-                    break;
-                default:
-                    System.out.println("Invalid command");
-                    break;
-                }
-        }
-        s.close();
-        return;
-    }
-
 
     public String getValidComand() {
         String result = null;
@@ -105,14 +48,11 @@ public class Viewer {
                     result = getValidComand();
                     break;
             }
-        //}
-        //s.close();
         return result;
 
     }
     public int getParameter(int max){
         int result;
-        //Scanner s = new Scanner(System.in);
         String input = s.nextLine();
         Scanner s1 = new Scanner(input);
         if(!s1.hasNextInt()){
@@ -124,8 +64,7 @@ public class Viewer {
                 System.out.println("Invalid option, please try again");
                 result = getParameter(max);
             }
-        }
-        //s.close();
+        } 
         s1.close();
         return result;
     }
