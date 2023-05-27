d37c6ddeec20251b536850df0fe6de13e73f3b38
Justice Baum <baumj2@wwu.edu>
2023-05-27 12:31:58 -0700

    Playing with GUI

--- src/Deadwood.java
+++ src/Deadwood.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 public class Deadwood {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("How many players?");
-        int numPlayers = s.nextInt();
-        ProgressManager game = new ProgressManager(null, null, null);
-        game.setUpGame(numPlayers);
-        game.setUpDay(numPlayers);
-        int currentPlayer = 0;
-        System.out.println("It is Player"+ currentPlayer+ "'s turn");
-        //GUI view = new GUI();
-        while(game.takeATurn(game.getPlayer(currentPlayer))){
-            currentPlayer++;
-            if(currentPlayer == numPlayers){
-                currentPlayer = 0;
-            }
-            System.out.println();
-            System.out.println("It is Player"+ currentPlayer+ "'s turn");
-        }
-        // for (int i = 0; i < game.getPlayers().length; i++) {
-        //     Player current = game.getPlayer(i);
-        //     boolean turn = true;
+        // Scanner s = new Scanner(System.in);
+        // System.out.println("How many players?");
+        // int numPlayers = s.nextInt();
+        // ProgressManager game = new ProgressManager(null, null, null);
+        // game.setUpGame(numPlayers);
+        // game.setUpDay(numPlayers);
+        // int currentPlayer = 0;
+        // System.out.println("It is Player"+ currentPlayer+ "'s turn");
+        // //GUI view = new GUI();
+        // while(game.takeATurn(game.getPlayer(currentPlayer))){
+        //     currentPlayer++;
+        //     if(currentPlayer == numPlayers){
+        //         currentPlayer = 0;
+        //     }
+        //     System.out.println();
+        //     System.out.println("It is Player"+ currentPlayer+ "'s turn");
         // }
+        
+        // GUI Testing
+        GUI gui = new GUI();
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -8,6 +8,9 @@ public class GUI extends JFrame{
     // Fields
     // Labels
     JLabel boardLabel;
+    JLabel cardLabel;
+    JLabel playerLabel;
+    JLabel mLabel;
 
     // Buttons
     JButton bAct;
@@ -19,7 +22,8 @@ public class GUI extends JFrame{
 
     // Methods
     public GUI() {
-        
+        Integer argument;
+        // Set title & exit operation
         JFrame frame = new JFrame("Deadwood");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //add stuff 
@@ -30,10 +34,11 @@ public class GUI extends JFrame{
         ImageIcon boardIcon = new ImageIcon("images/board.jpg");
         boardLabel.setIcon(boardIcon);
         boardLabel.setBounds(0,0,boardIcon.getIconWidth(), boardIcon.getIconHeight());
+        bPane.add(boardLabel, (Integer) 0);
         
         bPane.setPreferredSize(new Dimension(boardIcon.getIconWidth(), boardIcon.getIconHeight()));
         bPane.setBorder(BorderFactory.createTitledBorder("Board"));
-        bPane.add(boardLabel, new Integer(1));
+        //bPane.add(boardLabel, (Integer) 1);
         //bPane.addMouseListener(new MouseListener());
 
         frame.pack();
